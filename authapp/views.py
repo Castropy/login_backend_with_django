cfae21586a2a66832944b9853135ad925f495f74
Castropy <randrescastroi@gmail.com>
2026-01-06 11:11:42 -0400

    Validacion de credenciales en login_usuario

--- authapp/views.py
+++ authapp/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+# authapp/views.py
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.contrib.auth import authenticate
+from rest_framework import status
 from rest_framework import generics
 from django.contrib.auth.models import User
 from .serializers import RegisterSerializer
@@ -7,4 +11,13 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
 
-# Create your views here.
+@api_view(['POST'])
+def login_usuario(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        return Response({"message": "Login exitoso"}, status=status.HTTP_200_OK)
+    else:
+        return Response({"error": "Credenciales inválidas"}, status=status.HTTP_400_BAD_REQUEST)
