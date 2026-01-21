c738e75d4017176f9f2d4b5a1167f816040f0e06
Castropy <randrescastroi@gmail.com>
2026-01-20 20:23:11 -0400

    host local para web y para android studio

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-vm2@jcg*ekia105#9!2e8c2y2to)-w6r&uz=l9ts1-e!09t!dy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['10.0.2.2', 'localhost', '127.0.0.1']
 
 
 # Application definition
